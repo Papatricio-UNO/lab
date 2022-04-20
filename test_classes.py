d0e5dba97d41ef55d3396080f84643fee3eaf1e6
Papatricio-UNO <patparonable@gmail.com>
2022-04-20 11:14:47 -0500

    Changed unittest to pytest

--- test_classes.py
+++ test_classes.py
@@ -1,11 +1,5 @@
-import unittest
+import pytest
 from classes import *
 
 
-class MyTestCase(unittest.TestCase):
-    def setUp(self) -> None:
-        pass
 
-
-if __name__ == '__main__':
-    unittest.main()
