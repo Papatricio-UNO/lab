17d5cbc7d6ea5f757ecb26a270ffa3f8af1487b9
Papatricio-UNO <patparonable@gmail.com>
2022-04-20 11:18:50 -0500

    Added methods from the Television class

--- test_classes.py
+++ test_classes.py
@@ -2,4 +2,21 @@ import pytest
 from classes import *
 
 
+def test_power():
+    pass
 
+
+def test_channel_up():
+    pass
+
+
+def test_channel_down():
+    pass
+
+
+def test_volume_up():
+    pass
+
+
+def test_volume_down():
+    pass
